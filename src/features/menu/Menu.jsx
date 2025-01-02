3c4bf0cfa1612d5954e93e6a59f5b61562f3aad0
marcin.chowaniec <marcin.chowaniec@asseco.pl>
2025-01-02 19:19:26 +0100

    first commit

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
+export default function Menu() {
+  const menu = useLoaderData();
+
+  return (
+    <ul>
+      {menu.map((item) => (
+        <MenuItem pizza={item} key={item.id}></MenuItem>
+      ))}
+    </ul>
+  );
+}
---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
+
+export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  console.log(navigation);
+  return (
+    <div className="layout">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <main>
+            <h1>Content</h1>
+            <Outlet />
+          </main>
+
+          <CartOverview />
+        </>
+      )}
+    </div>
+  );
+}
