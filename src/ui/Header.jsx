a323a660c615b901a289f62cc0461377584c55a2
marcin.chowaniec <marcin.chowaniec@asseco.pl>
2025-01-04 12:13:55 +0100

    Add error handling and search functionality for orders
    - Implemented error handling in routing with Error component.
    - Updated CreateOrder component to handle form submission and validation.
    - Added SearchOrder component for searching orders by ID.
    - Refactored Order component to load order data from API.

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 export default function Header() {
   return (
     <header>
       <Link to="/">Fast React Pizza Co.</Link>
-
+      <SearchOrder />
       <p>John</p>
     </header>
   );
