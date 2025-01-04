ce67dc8f9cebed9187868cfd1760c67b2af6775b
marcin.chowaniec <marcinchowaniec21@gmail.com>
2025-01-04 13:46:18 +0100

    Add configuration files for PostCSS, Prettier, and TailwindCSS; implement user interface components

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import Header from "./Header";
-import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "./Loader";
+import Header from './Header';
+import Loader from './Loader';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
 
-export default function AppLayout() {
+function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
-  return (
-    <div className="layout">
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          <Header />
-          <main>
-            <h1>Content</h1>
-            <Outlet />
-          </main>
+  const isLoading = navigation.state === 'loading';
 
-          <CartOverview />
-        </>
-      )}
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
+
+      <Header />
+
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+
+      <CartOverview />
     </div>
   );
 }
+
+export default AppLayout;
